f4f04014fe8b092a6f63a0f0c25536af8ffd8f29
rafawis <rafa.wisnutomo@gmail.com>
2025-09-03 13:21:47 +0700

    Complete tutorial 1: Django MVT implementation

---
+++ main/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+def show_main(request):
+    context = {
+        'npm' : '2406496290',
+        'name': 'Rafa Pradipta Ali Wisnutomo',
+        'class': 'PBP E'
+    }
+
+    return render(request, "main.html", context)
+
